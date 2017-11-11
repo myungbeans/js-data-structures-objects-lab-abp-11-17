eb2505de07c82b0777fa8620c561ba180066dd5d
Michael Myung <michael.h.myung@gmail.com>
2017-11-11 21:45:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,3 @@ function updateDriverWithKeyAndValue (obj, key, value){
 function destructivelyUpdateDriverWithKeyAndValue (obj, key, value){
   return Object.assign(obj, {[key]:value});
 }
-
-funciton deleteFromDriverByKey (obj, key, value){
-}
